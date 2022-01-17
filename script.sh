ad3f612d31f537eb17a8730e0dc6846976c4d374
teresalazar13 <teresa.sal13@gmail.com>
2022-01-17 00:11:29 +0000

    Adds results.

--- script.sh
+++ script.sh
@@ -62,13 +62,13 @@ done
 
 DATASET=adult
 
-#python3 main.py --dataset $DATASET --fl FedAvg --n_runs $N_RUNS --n_rounds $N_ROUNDS
-#python3 main.py --dataset $DATASET --fl FedAvgGR --n_runs $N_RUNS --n_rounds $N_ROUNDS
-#python3 main.py --dataset $DATASET --fl FedAvgLR --n_runs $N_RUNS --n_rounds $N_ROUNDS
+python3 main.py --dataset $DATASET --fl FedAvg --n_runs $N_RUNS --n_rounds $N_ROUNDS
+python3 main.py --dataset $DATASET --fl FedAvgGR --n_runs $N_RUNS --n_rounds $N_ROUNDS
+python3 main.py --dataset $DATASET --fl FedAvgLR --n_runs $N_RUNS --n_rounds $N_ROUNDS
 
 for BETA in 0.99 #0.7 0.8 0.9 0.99
 do
-	#python3 main.py --dataset $DATASET --fl FedMom --beta $BETA --n_runs $N_RUNS --n_rounds $N_ROUNDS
+	python3 main.py --dataset $DATASET --fl FedMom --beta $BETA --n_runs $N_RUNS --n_rounds $N_ROUNDS
 	for LAMBDA_ in 0.045 0.047 0.05 #0.035 0.04 0.045 0.047 0.05
 	do
 	  for METRICS in SP TPR EQO
