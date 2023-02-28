dc7198aacad19c98dfafc0ef4fe4b0dcca4d66f6
teresalazar13 <teresa.sal13@gmail.com>
2023-02-28 00:10:48 +0000

    Updates script.

--- script.sh
+++ script.sh
@@ -1,17 +1,17 @@
 N_RUNS=10
 N_ROUNDS=100
 DATASET=compas
-ALPHA=1.0
+ALPHA=0.5
 
-for BETA in 0.85 0.95 #0.8 0.9 0.99
+for BETA in 0.8 0.85 0.9 0.95 0.99
 do
-  for RHO in 0.02 0.03 0.06 #0.04 0.05
+  for RHO in 0.02 0.03 0.04 0.05 0.06
   do
-    for L0 in 0.2 0.3 0.4 #0.1 0.5
+    for L0 in 0.1 0.2 0.3 0.4 0.5
     do
-      for MAX in 0.85 0.95 #0.8 0.9 1.0
+      for MAX in 0.8 0.85 0.9 0.95 1.0
       do
-        for METRICS in SP TPR EQO
+        for METRICS in SP
         do
           python3 main.py --dataset $DATASET --fl FairFate --beta $BETA --rho $RHO --l0 $L0 --MAX $MAX --metrics $METRICS --n_runs $N_RUNS --n_rounds $N_ROUNDS --alpha $ALPHA
         done
